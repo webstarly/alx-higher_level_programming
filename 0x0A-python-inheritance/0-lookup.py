ed9ce58a0b639f40acc3abd41b739b35c2e1d52f
Edidiong Ukem <edidionge.ukem@gmail.com>
2023-01-09 19:49:32 -0800

    alx task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+
+Contains method lookup that returns list of object's attribute and methods
+
+"""
+
+
+def lookup(obj):
+
+    """returns list of object's attribute and methods"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+
+Contains empty class BaseGeometry
+
+"""
+
+
+class BaseGeometry:
+
+    """empty class"""
+
+    pass
